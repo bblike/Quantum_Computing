948affd30859776db90d49ae593936042e372d1e
bblike <lzjgnh2002@gmail.com>
2023-10-07 16:33:34 +0100

    add program goals

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print("Hello world!")
+"""
+    This program is for the task in computing project.
+    The goal of this program is to simulate particles with behaviours represented by Monte Carlo wave function.
+    Compare this with OBE mode to see the differences.
+    
+"""
