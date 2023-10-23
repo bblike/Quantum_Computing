fix a problem with Heff calculation

functions.py
@@ -3,7 +3,7 @@
 """
 import numpy as np
 import parameters as paras
-
+import decimal
 
 
 """
@@ -57,10 +57,3 @@ def daga(function):
     result = paras.jump * function
     return result
 
-
-x1 = np.matrix([[1,0],
-               [1,3]])
-x2 = np.matrix([[5],
-                [1]])
-
-print(x1*x2)

parameters.py
@@ -7,7 +7,7 @@ import random
 import cmath
 
 
-det = 0.1               #delta sign
+det = 0.10              #delta sign
 rabi = 0                #omega sign
 Gamma = 0.1 * rabi      #Gamma sign
 h = 6.626e-34           #planck constant
@@ -16,7 +16,7 @@ m = 9.11e-31            #mass of a electron
 hat = np.matrix([[1, 0],
                  [0, 1]])
 prob = np.array([3, 7]) #[a,b] represent for a/(a+b) with [1,0] and b/(a+b) with [0,1]
-delt = 0
+delt = 0.10000000000
 heff = hbar/2*np.matrix([[det, rabi],
                          [rabi, -det-(0+1j)*Gamma]])
 init = np.matrix([[0],